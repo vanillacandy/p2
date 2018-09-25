5be0c1b9d9f89566efc3255df3b6d83c17c0209d
Catherine Lin <cathy8295@gmail.com>
2018-09-25 18:49:28 -0400

    css

--- index.php
+++ index.php
@@ -9,22 +9,27 @@ require 'logic.php';
     <title>Hello World</title>
     <meta charset='utf-8'>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+          crossorigin="anonymous">
     <link rel='stylesheet' href='/styles/app.css'>
 </head>
 
 <body>
 <h1>Hello</h1>
 
-<ul class = 'books'>
-<?php foreach ($books as $title => $book) : ?>
-    <li class='book'>
-        <div class='book'>
-            <?= $title ?> by <?= $book['author'] ?>
-            <img src='<?= $book['cover_url'] ?>' alt='Cover photo for the book <?= $title ?>'
-        </div>
-    </li>
-<?php endforeach ?>
+<ul class='books'>
+    <?php foreach ($books as $title => $book) : ?>
+        <li class='book'>
+            <div class='title'><?= $title ?></div>
+            <div class='author'>
+                 by <?= $book['author'] ?>
+            </div>
+            <img src='<?= $book['cover_url'] ?>' alt='Cover photo for the book <?= $title ?>'>
+            
+        </li>
+    <?php endforeach ?>
 </ul>
 
 </body>
